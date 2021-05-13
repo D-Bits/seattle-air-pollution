09cbe4f73bdc5c5d272ddb0b9477108b52c245fd
D-Bits <lockwood.da88@gmail.com>
2021-05-13 11:07:57 -0700

    Added pollution table.

---
+++ dags/sql/tables.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE pollution
+(
+    id SERIAL PRIMARY KEY,
+    "date" DATE NOT NULL,
+    aqi DECIMAL NOT NULL,
+    co DECIMAL NOT NULL,
+    no2 DECIMAL NOT NULL,
+    o3 DECIMAL NOT NULL,
+    pm10 DECIMAL NOT NULL,
+    pm25 DECIMAL NOT NULL,
+    so2 DECIMAL NOT NULL,
+
+);
