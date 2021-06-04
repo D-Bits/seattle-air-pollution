1b509f1164165f6a63a89ba66cb2ddc541de57cf
D-Bits <lockwood.da88@gmail.com>
2021-06-04 12:15:30 -0700

    Added reusable settings for DAGs.

---
+++ settings.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+import os 
+
+
+# Settings to be used across DAGs
+
+# Get the API token from an environment variable 
+api_key = os.getenv("API_KEY")
+# Set latitude and longitude for Seattle
+lat = 47.6062
+lon = -122.3321
+
+# Create SQL Alchemy engine for loading data
+db_engine = create_engine(os.getenv("DB_CONN"))
