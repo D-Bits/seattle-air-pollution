65f213ffc9efd3b0fc302d98de2a5bf794010427
D-Bits <lockwood.da88@gmail.com>
2021-06-13 16:50:37 -0700

    Added new API key to global settings.

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ import os
 
 # Get the API token from an environment variable 
 open_weathermap_key = os.getenv("OPEN_WEATHER_MAP_KEY")
+weatherapi_key = os.getenv("WEATHERAPI_KEY")
 # Set latitude and longitude for Seattle
 lat = 47.6062
 lon = -122.3321
