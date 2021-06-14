cd07ec535521bc8c797eacb731a637ae10a8a7e7
D-Bits <lockwood.da88@gmail.com>
2021-06-14 13:32:08 -0700

    Added rainfall view

--- dags/sql/views.sql
+++ dags/sql/views.sql
@@ -1,6 +1,14 @@
 /* TODO: Write views for analysis */
 
+
+-- Show pollution data by AQI
 CREATE VIEW vw_highest_aqi AS 
 SELECT * 
 FROM public.pollution 
 ORDER BY aqi DESC;
+
+-- Show days with the most rainfall
+CREATE VIEW vw_highest_rainfall AS 
+SELECT * 
+FROM public.atmospheric
+ORDER BY precip_mm DESC;
