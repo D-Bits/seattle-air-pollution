34748c6583033f65dcf48e52e79af1fa76b4561f
D-Bits <lockwood.da88@gmail.com>
2021-06-28 01:12:04 -0700

    Added view for hottest dates.

--- dags/sql/views.sql
+++ dags/sql/views.sql
@@ -12,3 +12,9 @@ CREATE VIEW vw_highest_rainfall AS
 SELECT * 
 FROM public.atmospheric
 ORDER BY precip_mm DESC;
+
+-- Show hottest times
+CREATE VIEW vw_hottest AS 
+SELECT last_updated, temp_c
+FROM public.atmospheric 
+ORDER BY TEMP_C DESC;
